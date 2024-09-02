49298fb86306572ca7a72771df0bcaa8db2fbc1d
qBor <43086534+kevinfosse@users.noreply.github.com>
2024-09-02 23:36:44 +0200

    Update const.js

--- frontend-expo/const.js
+++ frontend-expo/const.js
@@ -3,5 +3,5 @@ import { Platform } from "react-native";
 //Version manp
 //export const API_URL = Platform.OS === "web" ? 'http://localhost:8888/public' : 'http://10.167.129.98:8888/public';
 //Version wamp
-export const API_URL = Platform.OS === "web" ? 'http://localhost/RessourcesRelationnelles/backend/public' : 'http://10.167.128.182/RessourcesRelationnelles/backend/public';
+export const API_URL = Platform.OS === "web" ? 'http://localhost/service' : 'http://10.167.128.182/RessourcesRelationnelles/backend/public';
 //Changer adresse ip pour mobile
